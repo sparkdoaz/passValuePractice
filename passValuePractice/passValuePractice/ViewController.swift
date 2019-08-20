ec60f412210f6b20fa04ca36a7f1dfeb9c725747
JC Spark <sparkdoaz@gmail.com>
2019-08-20 15:43:17 +0800

    base

--- passValuePractice/passValuePractice/ViewController.swift
+++ passValuePractice/passValuePractice/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //        tableView.rowHeight = UITableView.automaticDimension
+        
+        //        tableView.estimatedRowHeight = 300
     }
+    
+    
+}
 
+extension ViewController: UITableViewDelegate {
+    
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "firstCell", for: indexPath) as? FirstCell else { fatalError() }
+        cell.backgroundColor = .white
+        cell.setBord()
+        //        cell.deleteBtn.layer.borderColor = UIColor.black.cgColor
+        
+        return cell
+        
+    }
+    
+    
 }
 
