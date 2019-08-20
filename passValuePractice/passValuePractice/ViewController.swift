aa8f09b5e6aaaff7bd2e76c5ab14476c4f0e4707
JC Spark <sparkdoaz@gmail.com>
2019-08-20 15:33:13 +0800

    create project

---
+++ passValuePractice/passValuePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  passValuePractice
+//
+//  Created by 黃建程 on 2019/8/20.
+//  Copyright © 2019 Spark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
